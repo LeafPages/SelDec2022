6ad4c62d394c9d804ba2bb693a38fd52ce5bb32c
hariqeagle <95277174+hariqeagle@users.noreply.github.com>
2023-01-29 18:39:05 +0530

    testng day3

---
+++ week7/day2/RetryFailedTests.java
@@ -0,0 +1,18 @@
+package week7.day2;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class RetryFailedTests implements IRetryAnalyzer{
+	int retryCount = 0;
+
+	public boolean retry(ITestResult result) {
+		if(retryCount < 2) {
+			retryCount++;
+			return true;
+		}
+		
+		return false;
+	}
+
+}
